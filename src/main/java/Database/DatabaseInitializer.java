85e8da5eb4414bfd3f0cd16a82e374c9981968ac
Nazarii Ihnat <ignatnazar@gmail.com>
2018-11-03 19:20:30 +0300

    Created createPriorityTableIfNotExists() and addDataToPriorityTable() methods, renamed createTableIfNotExists() to createTaskTableIfNotExists() , updated createTaskTableIfNotExists() method.

--- src/main/java/Database/DatabaseInitializer.java
+++ src/main/java/Database/DatabaseInitializer.java
@@ -1,4 +1,4 @@
-package Dayabase;
+package Database;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -6,21 +6,55 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public class DatabaseInitializer {
+    public static void main(String[] args) {
+        DatabaseInitializer databaseInitializer = new DatabaseInitializer();
+    }
     private Connection connection;
     {
         try {
             String url = "jdbc:sqlite:Tasks.db";
             connection = DriverManager.getConnection(url);
-            createTableIfNotExists();
+            createPriorityTableIfNotExists();
+            createTaskTableIfNotExists();
         } catch (SQLException e) {
             e.printStackTrace();
         }
     }
 
-    private void createTableIfNotExists() throws SQLException {
+    private void createPriorityTableIfNotExists() throws SQLException {
+        String query = "CREATE TABLE IF NOT EXISTS task_priority " +
+                "(priority_letter VARCHAR(1)," +
+                "priority_description VARCHAR(30));";
+        Statement statement = connection.createStatement();
+        statement.execute(query);
+        addDataToPriorityTable();
+    }
+
+    private void addDataToPriorityTable() throws SQLException {
+        String queryForPriorityA ="INSERT INTO task_priority VALUES" +
+                "('A'," +
+                "'important and urgent');";
+        String queryForPriorityB ="INSERT INTO task_priority VALUES" +
+                "('B'," +
+                "'important and not urgent');";
+        String queryForPriorityC ="INSERT INTO task_priority VALUES" +
+                "('C'," +
+                "'unimportant and urgent');";
+        String queryForPriorityD ="INSERT INTO task_priority VALUES" +
+                "('D'," +
+                "'unimportant and nor urgent');";
+        String arrayOfQueries[] = {queryForPriorityA, queryForPriorityB, queryForPriorityC, queryForPriorityD};
+        Statement statement = connection.createStatement();
+        for (String query : arrayOfQueries)
+            statement.execute(query);
+    }
+
+    private void createTaskTableIfNotExists() throws SQLException {
         String query = "CREATE TABLE IF NOT EXISTS task " +
-                "(id INT," +
-                "desc TEXT);";
+                "(id INT AUTO_INCREMENT PRIMARY KEY," +
+                "description TEXT," +
+                "priority VARCHAR(1)," +
+                "FOREIGN KEY(priority) REFERENCES task_priority (priority_letter));";
         Statement statement = connection.createStatement();
         statement.execute(query);
     }
