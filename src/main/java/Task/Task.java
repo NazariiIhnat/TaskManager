24bbab3ed8605532b636a23807512e8c45ac8d82
Nazarii Ihnat <ignatnazar@gmail.com>
2018-11-03 16:24:46 +0300

    Created Task class in Task package with 3 private fields and constructer

---
+++ src/main/java/Task/Task.java
@@ -0,0 +1,20 @@
+package Task;
+
+import java.util.Scanner;
+
+public class Task {
+    private String date;
+    private String taskDescription;
+    private String taskPriority;
+    private Scanner scanner;
+
+    public Task(){
+        scanner = new Scanner(System.in);
+        System.out.println("Set the start day of your task.");
+        date = scanner.nextLine();
+        System.out.println("Describe your task.");
+        taskDescription = scanner.nextLine();
+        System.out.println("Set priority of task from A to D");
+        taskPriority = scanner.nextLine();
+    }
+}
