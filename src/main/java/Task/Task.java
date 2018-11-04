14a737e0e4e934f65165867304b4c96f5ea379d3
Nazarii Ihnat <ignatnazar@gmail.com>
2018-11-04 14:50:28 +0300

    Created main method in Task class(it's unnecesarry for now), created 3 objects in Task constructure, scanner.close() and adding this object to methot whitch add him to db.

--- src/main/java/Task/Task.java
+++ src/main/java/Task/Task.java
@@ -1,21 +1,32 @@
 package Task;
 
+import Database.*;
+
+import java.sql.SQLException;
 import java.util.Scanner;
 
 public class Task {
     private String date;
     private String taskDescription;
     private String taskPriority;
-    private Scanner scanner;
 
-    public Task(){
-        scanner = new Scanner(System.in);
+    public static void main(String[] args) throws SQLException {
+        Task task = new Task();
+    }
+
+    public Task() throws SQLException {
+        DatabaseInitializer databaseInitializer = new DatabaseInitializer();
+        Scanner scanner = new Scanner(System.in);
+        TaskDatabaseModifier taskDatabaseModifier = new TaskDatabaseModifier();
+
         System.out.println("Set the start day of your task.");
         date = scanner.nextLine();
         System.out.println("Describe your task.");
         taskDescription = scanner.nextLine();
         System.out.println("Set priority of task from A to D");
         taskPriority = scanner.nextLine();
+        scanner.close();
+        taskDatabaseModifier.addTaskToDatabase(this);
     }
 
     public String getDate() {
