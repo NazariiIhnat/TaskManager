ad85979e48e6357aab9ce5987fb565df8a5d729c
Nazarii Ihnat <ignatnazar@gmail.com>
2018-11-08 22:35:50 +0300

    Deleted deleteTaskFromDatabase(Task task) method.

--- src/main/java/Database/TaskDatabaseModifier.java
+++ src/main/java/Database/TaskDatabaseModifier.java
@@ -6,30 +6,19 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class TaskDatabaseModifier {
+    private DatabaseInitializer databaseInitializer = new DatabaseInitializer();
 
     public void addTaskToDatabase(Task task) throws SQLException {
         String query = "INSERT INTO task VALUES" +
                 "(?," +
                 "?," +
                 "?);";
-        PreparedStatement preparedStatement = DatabaseInitializer.CONNECTION.prepareStatement(query);
+        PreparedStatement preparedStatement = databaseInitializer.connection.prepareStatement(query);
         preparedStatement.setString(1, task.getDate());
         preparedStatement.setString(2, task.getTaskDescription());
         preparedStatement.setString(3, task.getTaskPriority());
         preparedStatement.executeUpdate();
         System.out.println("Task was added");
     }
-
-    public void deleteTaskFromDatabase(Task task) throws SQLException {
-        String query = "DELETE FROM task WHERE " +
-                "date = ? AND " +
-                "description = ? AND " +
-                "priority = ?;";
-        PreparedStatement preparedStatement = DatabaseInitializer.CONNECTION.prepareStatement(query);
-        preparedStatement.setString(1, task.getDate());
-        preparedStatement.setString(2, task.getTaskDescription());
-        preparedStatement.setString(3, task.getTaskPriority());
-        preparedStatement.executeUpdate();
-        System.out.println("Task was deleted");
-    }
+    //delete task from db method
 }
