5e4e486deb8292b9b32f219d2573733361f02c19
Nazarii Ihnat <ignatnazar@gmail.com>
2018-11-08 22:40:50 +0300

    Updated isPastDay(String inputData) and isDate (String inputData) methods. Class DataVerifier is public.

--- src/main/java/Utilites/DataVerifier.java
+++ src/main/java/Utilites/DataVerifier.java
@@ -5,11 +5,11 @@ import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.format.DateTimeParseException;
 
-class DataVerifier {
+public class DataVerifier {
     boolean isDate(String inputData) {
         try {
             new SimpleDateFormat("yyyy-MM-dd").parse(inputData); //checks if in format yyyy-MM-dd. If not - catched by ParseException.
-            return !isPastDay(LocalDate.parse(inputData)); //return false if day is past and LocalDate.parse(inputData) return true if day is valid. If not - catched by  DateTimeParseException.
+            LocalDate.parse(inputData); //return false if day is past and LocalDate.parse(inputData) return true if day is valid. If not - catched by  DateTimeParseException.
         } catch (ParseException e) {
             System.out.println("Date format error. Write in format yyyy-MM-dd.");
             return false;
@@ -18,10 +18,11 @@ class DataVerifier {
             System.out.println("Invalid date " + inputData + ".");
             return false;
         }
+        return true;
     }
 
-    private boolean isPastDay(LocalDate inputData){ //поправити код
-        if(inputData.isBefore(LocalDate.now())){
+    public boolean isPastDay(String inputData){ //поправити код
+        if(LocalDate.parse(inputData).isBefore(LocalDate.now())){
             System.out.println("You can't add task to past days. Today is " + LocalDate.now());
             return true;
         }
