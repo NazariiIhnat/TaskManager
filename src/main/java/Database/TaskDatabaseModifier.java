7a85ea4bc425c75aa797a7dad7291394e8c75dbd
Nazarii Ihnat <ignatnazar@gmail.com>
2018-11-15 01:04:40 +0300

    Created deleteTaskFromDatabase() and isExistingID(int rowid) methods in  TaskDatabaseModifier calss.

--- src/main/java/Database/TaskDatabaseModifier.java
+++ src/main/java/Database/TaskDatabaseModifier.java
@@ -1,12 +1,16 @@
 package Database;
 
 import Task.Task;
+import Utilites.LoopScanner;
 
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class TaskDatabaseModifier {
     private DatabaseInitializer databaseInitializer = new DatabaseInitializer();
+    private LoopScanner loopScanner = new LoopScanner();
 
     public void addTaskToDatabase(Task task) throws SQLException {
         String query = "INSERT INTO task VALUES" +
@@ -20,5 +24,30 @@ public class TaskDatabaseModifier {
         preparedStatement.executeUpdate();
         System.out.println("Task was added");
     }
-    //delete task from db method
+
+    public void deleteTaskFromDatabase() throws SQLException {
+        int rowid = loopScanner.readID();
+        if (isExistingID(rowid)) {
+            String query = "DELETE FROM task WHERE rowid = " + rowid + ";";
+            PreparedStatement preparedStatement = databaseInitializer.connection.prepareStatement(query);
+            preparedStatement.executeUpdate();
+            System.out.println("Task was deleted.");
+        }
+    }
+
+    private boolean isExistingID(int rowid) {
+        String query = "SELECT rowid FROM task " +
+                "WHERE rowid = " + rowid +";";
+        try {
+            Statement statement = databaseInitializer.connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(query);
+            resultSet.getInt(1);
+            return true;
+        } catch (SQLException e) {
+            System.out.println("Task with id " + rowid + " doesn't exist.");
+            return false;
+        }
+    }
+
+    //методи для зміни даних завдання
 }
