1e995031818b0b6ce1924ef37cc9fb00469d6605
Nazarii Ihnat <ignatnazar@gmail.com>
2018-11-22 21:04:12 +0300

    TaskSearcher is extending AbstractManager, searchTasks(String subQuery) and void showTasks(ResultSet resultSet, ResultSetMetaData resultSetMetaData) methods deleted from TaskSearcher class, updated all searching methods

--- src/main/java/TaskManagement/TasksSearcher.java
+++ src/main/java/TaskManagement/TasksSearcher.java
@@ -1,65 +1,44 @@
-package TaskMenegment;
+package TaskManagement;
 
-import Database.DatabaseInitializer;
 import Utilites.LoopScanner;
 
 import java.sql.*;
 
-public class TasksSearcher {
-    private  DatabaseInitializer databaseInitializer = new DatabaseInitializer();
+public class TasksSearcher extends AbstractManager {
     private  LoopScanner loopScanner = new LoopScanner();
 
     public static void main(String[] args) throws SQLException {
         TasksSearcher tasksSearcher = new TasksSearcher();
-        tasksSearcher.showTaskByID();
+        tasksSearcher.showAllTasks();
     }
 
     public void searchTasksByDate() throws SQLException {
-        String subQuery = "start_date = '" + loopScanner.readDate() + "';";
+        String subQuery = " WHERE start_date = '" + loopScanner.readDate() + "';";
         searchTasks(subQuery);
     }
 
     public void searchTasksByPriority() throws SQLException {
-        String subQuery = "priority = '" + loopScanner.readPriority() + "';";
+        String subQuery = " WHERE priority = '" + loopScanner.readPriority() + "';";
         searchTasks(subQuery);
     }
 
     public void searchTasksByPartOfDescription() throws SQLException {
-        String subQuery = "description LIKE '%" + loopScanner.readDescription() + "%';";
+        String subQuery = " WHERE description LIKE '%" + loopScanner.readDescription() + "%';";
         searchTasks(subQuery);
     }
 
     public void searchTasksByRangeOfDate() throws SQLException {
-        String subQuery = "start_date BETWEEN '" + loopScanner.readDate() + "' AND '" + loopScanner.readDate() + "';";
+        String subQuery = " WHERE start_date BETWEEN '" + loopScanner.readDate() + "' AND '" + loopScanner.readDate() + "';";
         searchTasks(subQuery);
     }
 
-    public void showTaskByID() throws SQLException {
-        String subQuery = "rowid = " + loopScanner.readID() + ";";
+    public void searchTaskByID() throws SQLException {
+        String subQuery = " WHERE rowid = " + loopScanner.readID() + ";";
         searchTasks(subQuery);
     }
 
-    private void searchTasks(String subQuery) throws SQLException {
-        String query = "SELECT rowid, start_date, description, priority FROM task " +
-                "WHERE " + subQuery;
-
-        Statement statement = databaseInitializer.connection.createStatement();
-        ResultSet resultSet = statement.executeQuery(query);
-        ResultSetMetaData resultSetMetaData = resultSet.getMetaData();
-        showTasks(resultSet, resultSetMetaData);
-    }
-
-    private void showTasks(ResultSet resultSet, ResultSetMetaData resultSetMetaData) throws SQLException {
-
-        int columnsNumber = resultSetMetaData.getColumnCount();
-        int counter = 1;
-
-        while (resultSet.next()){
-            System.out.print(counter++ + ". ");
-            for(int i = 1; i<= columnsNumber; i++)
-                System.out.print(resultSet.getString(i) + " ");
-            System.out.println();
-        }
-        System.out.println();
+    public void showAllTasks() throws  SQLException {
+        String subQuery = ";";
+        searchTasks(subQuery);
     }
 }
