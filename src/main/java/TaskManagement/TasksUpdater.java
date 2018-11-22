f38524d2f6dbc1bc89201b299006c3a03b430935
Nazarii Ihnat <ignatnazar@gmail.com>
2018-11-22 21:06:41 +0300

    all updating methods of TaskUpdater class updated, deleted updateTask(String columnName, String value, int rowid) method

--- src/main/java/TaskManagement/TasksUpdater.java
+++ src/main/java/TaskManagement/TasksUpdater.java
@@ -1,92 +1,35 @@
-package TaskMenegment;
+package TaskManagement;
 
-import Database.DatabaseInitializer;
 import TaskObject.Task;
-import Utilites.DataVerifier;
 import Utilites.LoopScanner;
 
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 
-public class TasksUpdater {
-    private DatabaseInitializer databaseInitializer = new DatabaseInitializer();
+public class TasksUpdater extends AbstractManager {
     private LoopScanner loopScanner = new LoopScanner();
-    private DataVerifier dataVerifier = new DataVerifier();
 
     public static void main(String[] args) throws SQLException {
-        TasksUpdater taskDatabaseModifier = new TasksUpdater();
-        taskDatabaseModifier.updateTaskDescription();
+        TasksUpdater tasksUpdater = new TasksUpdater();
+        tasksUpdater.updateTaskDescription();
     }
 
     public void addTaskToDatabase(Task task) throws SQLException {
-        String query = "INSERT INTO task VALUES" +
-                "(?," +
-                "?," +
-                "?);";
-        PreparedStatement preparedStatement = databaseInitializer.connection.prepareStatement(query);
-        preparedStatement.setString(1, task.getDate());
-        preparedStatement.setString(2, task.getTaskDescription());
-        preparedStatement.setString(3, task.getTaskPriority());
-        preparedStatement.executeUpdate();
-        System.out.println("TaskObject was added");
+        super.addTask(task);
     }
 
     public void deleteTaskFromDatabase() throws SQLException {
-        int rowid = loopScanner.readID();
-        if (isExistingID(rowid)) {
-            String query = "DELETE FROM task WHERE rowid = " + rowid + ";";
-            PreparedStatement preparedStatement = databaseInitializer.connection.prepareStatement(query);
-            preparedStatement.executeUpdate();
-            System.out.println("Task was deleted.");
-        }
-    }
-
-    private boolean isExistingID(int rowid) {
-        String query = "SELECT rowid FROM task " +
-                "WHERE rowid = " + rowid +";";
-        try {
-            Statement statement = databaseInitializer.connection.createStatement();
-            ResultSet resultSet = statement.executeQuery(query);
-            resultSet.getInt(1);
-            return true;
-        } catch (SQLException e) {
-            System.out.println("Task with ID " + rowid + " doesn't exist.");
-            return false;
-        }
+        super.deleteTask();
     }
 
     public void updateTaskDate() throws SQLException {
-        String columnName = "start_date";
-        String value;
-        int rowid;
-        System.out.println("Set new start date");
-        do {
-            value = loopScanner.readDate();
-        } while (dataVerifier.isPastDay(value));
-        System.out.println("of task ID");
-        rowid = loopScanner.readID();
-        if(isExistingID(rowid))
-            updateTask(columnName, value, rowid);
+        updateTask("start_date", loopScanner.readDate(), loopScanner.readID());
     }
 
     public void updateTaskDescription() throws SQLException {
-        String columnName = "description";
-        String value;
-        int rowid;
-        System.out.println("Set new description ");
-        value = loopScanner.readDescription();
-        System.out.println("of task ID ");
-        rowid = loopScanner.readID();
-        if(isExistingID(rowid))
-            updateTask(columnName, value, rowid);
+        updateTask("description", loopScanner.readDescription(), loopScanner.readID());
     }
 
-    private void updateTask(String columnName, String value, int rowid) throws SQLException {
-        String query = "UPDATE task SET " + columnName + " = '" + value + "' WHERE rowid = " + rowid;
-
-        PreparedStatement preparedStatement = databaseInitializer.connection.prepareStatement(query);
-        preparedStatement.executeUpdate();
+    public void updateTaskPriority() throws SQLException {
+        updateTask("priority", loopScanner.readPriority(), loopScanner.readID());
     }
 }
