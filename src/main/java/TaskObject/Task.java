4b8d9f1f2e2b6a66fd01747eb0d4daf97b212974
Nazarii Ihnat <ignatnazar@gmail.com>
2018-11-25 23:39:26 +0300

    Deleted and renamed some private states. Updated constructure with 4 arguments. Updated getters, setters and toString() methods.

--- src/main/java/TaskObject/Task.java
+++ src/main/java/TaskObject/Task.java
@@ -6,69 +6,50 @@ import Utilites.LoopScanner;
 import java.sql.SQLException;
 
 public class Task {
-    private LoopScanner loopScanner = new LoopScanner();
-    private DataVerifier dataVerifier = new DataVerifier();
-    private TasksUpdater tasksUpdater = new TasksUpdater();
     private String id;
-    private String date;
-    private String taskDescription;
-    private String taskPriority;
+    private String startDate;
+    private String description;
+    private String priority;
+    private TasksUpdater tasksUpdater;
 
-    public Task() throws SQLException {
-        do {
-            date = loopScanner.readDate();
-        }while (dataVerifier.isPastDay(date));
-        taskDescription = loopScanner.readDescription();
-        taskPriority = loopScanner.readPriority();
+    public Task(String startDate, String description, String priority) throws SQLException {
+        tasksUpdater = new TasksUpdater();
+        this.startDate = startDate;
+        this.description = description;
+        this.priority = priority;
         tasksUpdater.addTaskToDatabase(this);
     }
 
     public Task(String id, String date, String taskDescription, String taskPriority) {
         this.id = id;
-        this.date = date;
-        this.taskDescription = taskDescription;
-        this.taskPriority = taskPriority;
+        this.startDate = date;
+        this.description = taskDescription;
+        this.priority = taskPriority;
     }
 
     public String getDate() {
-        return date;
+        return startDate;
     }
 
-    public String getTaskDescription() {
-        return taskDescription;
+    public String getDescription() {
+        return description;
     }
 
     public String getTaskPriority() {
-        return taskPriority;
+        return priority;
     }
 
     public String getId() {
         return id;
     }
 
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public void setDate(String date) {
-        this.date = date;
-    }
-
-    public void setTaskDescription(String taskDescription) {
-        this.taskDescription = taskDescription;
-    }
-
-    public void setTaskPriority(String taskPriority) {
-        this.taskPriority = taskPriority;
-    }
-
     @Override
     public String toString() {
         return "Task{" +
                 "id=" + id +
-                ", date='" + date + '\'' +
-                ", taskDescription='" + taskDescription + '\'' +
-                ", taskPriority='" + taskPriority + '\'' +
+                ", date='" + startDate + '\'' +
+                ", description='" + description + '\'' +
+                ", taskPriority='" + priority + '\'' +
                 '}';
     }
 }
