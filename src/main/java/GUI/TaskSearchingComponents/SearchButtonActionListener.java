919e872d353702debf737ccff08e13a6fac9744a
Nazarii Ihnat <ignatnazar@gmail.com>
2018-12-03 00:32:47 +0300

    Updated action listener in SearchButtonActionListener class

--- src/main/java/GUI/TaskSearchingComponents/SearchButtonActionListener.java
+++ src/main/java/GUI/TaskSearchingComponents/SearchButtonActionListener.java
@@ -1,7 +1,6 @@
 package GUI.TaskSearchingComponents;
 
 import GUI.TaskTableObject.TaskTable;
-import GUI.TextFieldsObject.TextField;
 import TaskManagement.TasksSearcher;
 
 import java.awt.event.ActionListener;
@@ -9,11 +8,10 @@ import java.sql.SQLException;
 
 class SearchButtonActionListener {
     private TasksSearcher tasksSearcher = new TasksSearcher();
-    private TaskTable taskTable = new TaskTable();
     private ActionListener searchButtonActionListener = e -> {
         try {
-            tasksSearcher.searchTasksByPriority(TextField.getSerchByPriorityTextField().getText().toUpperCase());
-            taskTable.refreshTable();
+            tasksSearcher.searchTasksByPriority(TaskSearchTextField.getSearchByPriorityTextField().getText().toUpperCase());
+            new TaskTable().refreshTable();
         } catch (SQLException e1) {
             e1.printStackTrace();
         }
