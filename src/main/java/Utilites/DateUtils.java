bac34683e9efd80e01b564a1381485c134b9f754
Nazarii Ihnat <ignatnazar@gmail.com>
2018-12-15 22:22:30 +0300

    Created getDate(JXDatePicker calendar), enableToChoosePastDays(JXDatePicker calendar, boolean choice), enableDateEdition(JXDatePicker calendar, boolean flag), nullifyCalendar(JXDatePicker calendar) in DateUtils class.

--- src/main/java/Utilites/DateUtils.java
+++ src/main/java/Utilites/DateUtils.java
@@ -1,4 +1,31 @@
 package Utilites;
 
+import org.jdesktop.swingx.JXDatePicker;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 public class DateUtils {
+    public static String getDate(JXDatePicker calendar) {
+        return new SimpleDateFormat("yyyy-MM-dd").format(calendar.getDate());
+    }
+
+    public static void enableToChoosePastDays(JXDatePicker calendar, boolean choice) {
+        Calendar cal = calendar.getMonthView().getCalendar();
+        cal.setTime(new Date());
+        if(!choice) {
+            calendar.getMonthView().setLowerBound(cal.getTime());
+        } else {
+            calendar.getMonthView().setLowerBound(null);
+        }
+    }
+
+    public static void enableDateEdition(JXDatePicker calendar, boolean flag) {
+        calendar.getEditor().setEditable(flag);
+    }
+
+    public static void nullifyCalendar(JXDatePicker calendar) {
+        calendar.setDate(null);
+    }
 }
