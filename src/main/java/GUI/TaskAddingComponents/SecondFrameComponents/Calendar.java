e98ec3326fd4e07bf4ddc634a970279b149b701a
Nazarii Ihnat <ignatnazar@gmail.com>
2018-12-15 23:19:13 +0300

    Disable to choose past days when add tasks

--- src/main/java/GUI/TaskAddingComponents/SecondFrameComponents/Calendar.java
+++ src/main/java/GUI/TaskAddingComponents/SecondFrameComponents/Calendar.java
@@ -1,9 +1,14 @@
 package GUI.TaskAddingComponents.SecondFrameComponents;
 
+import Utilites.DateUtils;
 import org.jdesktop.swingx.JXDatePicker;
 
 class Calendar {
     private static JXDatePicker jxDatePicker = new JXDatePicker();
+
+    static {
+        DateUtils.enableToChoosePastDays(jxDatePicker, false);
+    }
 
     static JXDatePicker getJxDatePicker() {
         return jxDatePicker;
